2927b3eef595ac7c136d971366688b122bde43d7
Tobi Shitta-Bey <tsbmedia@Tobis-Mac-mini.local>
2021-02-17 08:33:09 +0000

    feat[home] : grid layout impl

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,10 +6,19 @@ export default function Home() {
     <div className="home-container">
       <div className="screen landing">
         <div className="navbar-placeholder" />
+        <div className="screen-container">
+          <div className="landing-top">
+            <h1>Do incididunt dolor officia veniam nisi sit ipsum aute.</h1>
+          </div>
+          <div className="landing-bottom">r</div>
+        </div>
+      </div>
+      <div className="screen gardening">
         <div className="screen-container"></div>
       </div>
-      <div className="screen gardening">e</div>
-      <div className="screen cleaning">e</div>
+      <div className="screen cleaning">
+        <div className="screen-container"></div>
+      </div>
     </div>
   );
 }
