ec06da19865cd819f1d27fb1b2fe05e72cf6f14f
Tobi Shitta-Bey <tsbmedia@Tobis-Mac-mini.local>
2021-02-17 11:05:39 +0000

    feat [navbar] : grid x flex impl

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,5 +1,11 @@
 import React from "react";
-
-export default function index() {
-  return <div></div>;
+import "./styles.css";
+export default function Navbar() {
+  return (
+    <nav className="navbar-container">
+      <div className="left">tesdt</div>
+      <div className="main">tesdt</div>
+      <div className="right">tesdt</div>
+    </nav>
+  );
 }
