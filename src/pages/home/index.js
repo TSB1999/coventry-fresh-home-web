efa6745e959fcd005968b85f65ebc5b212a436ef
Tobi Shitta-Bey <tsbmedia@Tobis-Mac-mini.local>
2021-02-17 09:04:22 +0000

    feat[home-gardening] : gardening screen layout structure done

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,17 +6,22 @@ export default function Home() {
     <div className="home-container">
       <div className="screen landing">
         <div className="navbar-placeholder" />
-        <div className="screen-container">
+        <div className="screen-container column">
           <div className="landing-top">
-            <h1>Do incididunt dolor officia veniam nisi sit ipsum aute.</h1>
+            <h1>Domestic Cleaning and Gardening Services</h1>
           </div>
-          <div className="landing-bottom">r</div>
+          <div className="landing-bottom" />
         </div>
       </div>
       <div className="screen gardening">
-        <div className="screen-container"></div>
+        <div className="navbar-placeholder" />
+        <div className="screen-container row">
+          <div className="landing-left">re</div>
+          <div className="landing-right"></div>
+        </div>
       </div>
       <div className="screen cleaning">
+        <div className="navbar-placeholder" />
         <div className="screen-container"></div>
       </div>
     </div>
