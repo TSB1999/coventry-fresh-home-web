f2e07fe0ffa133294c0d05c31d7d4b337bc429b1
Tobi Shitta-Bey <tsbmedia@Tobis-Mac-mini.local>
2021-02-21 21:16:33 +0000

    form string encoded

--- src/pages/payment-portal/index.js
+++ src/pages/payment-portal/index.js
@@ -1,120 +1,87 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import "./styles.css";
+
+const Cryptr = require("cryptr");
+const cryptr = new Cryptr("myTotalySecretKey");
 
 export default function Portal() {
   const [priceData, setPriceData] = useState({});
   const [url, setUrl] = useState("");
 
-  function sendGardeningEmail(e) {
+  useEffect(() => {
+    console.log(priceData);
+  }, [priceData]);
+
+  function genURL(e) {
     e.preventDefault();
-    console.log(e.target, "vbreou");
+
+    const data = {
+      name: e.target[0].value,
+      package: e.target[1].value,
+      price: e.target[2].value,
+    };
+    setPriceData(data);
+
+    const encryptedString = cryptr.encrypt(JSON.stringify(data));
+    setUrl(encryptedString);
+    const decryptedString = cryptr.decrypt(encryptedString);
+
+    console.log("enc" + encryptedString);
+    console.log("dec" + decryptedString);
   }
 
   return (
     <div className="payment-portal">
-      <Form
-        style={{
-          display: "flex",
-          flex: "1",
-          flexDirection: "column",
-          color: "#919d5d",
-          fontWeight: "bold",
-          fontSize: 18,
-          textShadow: "1px 1px #a87c3d",
-          textAlign: "center",
-          textTransform: "uppercase",
-          backgroundColor: "#fff",
-          opacity: 1,
-          padding: "0 5rem 0 5rem",
-        }}
-        onSubmit={sendGardeningEmail}
-      >
-        <Form.Group
-          controlId="exampleForm.ControlInput1"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            flex: 1,
-            // backgroundColor: "red",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Form.Label> cutomer name (paste here)</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            // placeholder="David Keys"
-            style={{
-              color: "#979F8B",
-              fontWeight: "bold",
-              textTransform: "uppercase",
-              fontSize: 12,
-              textAlign: "center",
-            }}
-          />
-        </Form.Group>
-        <Form.Group
-          controlId="exampleForm.ControlInput1"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            flex: 1,
-            // backgroundColor: "blue",
-          }}
-        >
-          <Form.Label>package</Form.Label>
-          <Form.Control as="select">
-            <option>Silver</option>
-            <option>Gold</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group
-          controlId="exampleForm.ControlTextarea1"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            flex: "1",
-          }}
-        >
-          <Form.Label>price (number only - without currency sign)</Form.Label>
-          <Form.Control
-            as="textarea"
-            name="message"
-            rows={3}
-            // placeholder="Make your enquiry"
-            style={{
-              color: "#979F8B",
-              fontWeight: "bold",
-              textTransform: "uppercase",
-              fontSize: 12,
-              textAlign: "center",
-            }}
-          />
-        </Form.Group>
+      <div className="navbar-placeholder" />
+      <div className="screen-container column">
+        <div className="portal-form">
+          <Form className="admin-form" onSubmit={(e) => genURL(e)}>
+            <Form.Group controlId="exampleForm.ControlInput9">
+              <Form.Label> cutomer name (paste here)</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                // placeholder="David Keys"
+              />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlInput9">
+              <Form.Label>package</Form.Label>
+              <Form.Control as="select">
+                <option>Silver</option>
+                <option>Gold</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea9">
+              <Form.Label>
+                price (number only - without currency sign)
+              </Form.Label>
+              <Form.Control
+                type="text"
+                name="price"
+                // placeholder="David Keys"
+              />
+            </Form.Group>
 
-        <button
-          type="submit"
-          class="btn btn-primary"
-          style={{
-            width: "100%",
-            fontWeight: "bold",
-            //   backgroundColor: "transparent",
-            borderColor: "#fff",
-            fontSize: 13,
-            //   color: "#fff",
-          }}
-        >
-          GENERATE
-        </button>
-      </Form>
+            <button type="submit" class="btn btn-primary">
+              GENERATE
+            </button>
+          </Form>
 
-      <h1>{"url address : " + url}</h1>
+          <Form.Group
+            controlId="exampleForm.ControlTextarea9"
+            className="url-code"
+          >
+            <Form.Label>url</Form.Label>
+            <Form.Control
+              type="text"
+              name="url"
+              value={url}
+              // placeholder="David Keys"
+            />
+          </Form.Group>
+        </div>
+      </div>
     </div>
   );
 }
