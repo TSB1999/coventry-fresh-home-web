d5ff8b9155a08322cbe29e6e22cf086b97d0c822
Tobi Shitta-Bey <tsbmedia@Tobis-Mac-mini.local>
2021-03-03 21:32:11 +0000

    contact impl

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -9,138 +9,156 @@ export default function index() {
     <div>
       <Navbar />
 
-      <div className="screen gallery-landing" style={{ height: "91vh" }}>
+      <div
+        className="screen gallery-landing"
+        style={{ height: "91vh", backgroundColor: "#3ab34a" }}
+      >
         <div
           className="screen-container column"
           style={{
             height: "91vh",
+            flexDirection: "row",
+            // backgroundColor: "purple",
           }}
         >
           <div
             style={{
-              display: "flex",
               flex: 1,
-              margin: "-2vh",
-              flexDirection: "column",
+              display: "grid",
+              backgroundColor: "#3ab34a",
             }}
           >
-            <div className="contact-flex-container">
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <div
-                className="contact-column-1"
-                style={{ padding: "10vh 0 10vh 10vh" }}
+                style={{
+                  display: "flex",
+                  flex: 1,
+                  // backgroundColor: "red",
+                  justifyContent: "center",
+                }}
               >
-                <div
-                  className="contact-column-1-1"
-                  activeClass="active"
-                  to="gallery-bathroom"
-                  spy={true}
-                  smooth={true}
-                  hashSpy={true}
-                  duration={500}
-                  isDynamic={true}
-                  ignoreCancelEvents={false}
-                >
-                  <h1
-                    style={{
-                      fontSize: "3rem",
-                      fontWeight: "bold",
-                      color: "#3ab34a",
-                      textShadow: "0.2rem 0.2rem #000",
-                      textTransform: "uppercase",
-                      textAlign: "center",
-                      writingMode: "vertical-rl",
-                      padding: "1rem",
-                    }}
-                  >
-                    {/* Bathrooms */}
-                  </h1>
-                </div>
-                <div
-                  className="contact-column-1-2"
-                  activeClass="active"
-                  to="gallery-bedroom"
-                  spy={true}
-                  smooth={true}
-                  hashSpy={true}
-                  duration={500}
-                  isDynamic={true}
-                  ignoreCancelEvents={false}
-                >
-                  <h1
-                    style={{
-                      fontSize: "3rem",
-                      fontWeight: "bold",
-                      color: "#3ab34a",
-                      textShadow: "0.2rem 0.2rem #000",
-                      textTransform: "uppercase",
-                      textAlign: "center",
-                      writingMode: "vertical-rl",
-                      padding: "1rem",
-                    }}
-                  >
-                    {/* Bedrooms */}
-                  </h1>
-                </div>
+                <i class="fab fa-instagram fa-2x"></i>
               </div>
-              <div
-                className="contact-column-2"
-                style={{ padding: "10vh 10vh 10vh 0vh" }}
-              >
-                <div
-                  className="contact-column-2-1"
-                  activeClass="active"
-                  to="gallery-kitchen"
-                  spy={true}
-                  smooth={true}
-                  hashSpy={true}
-                  duration={500}
-                  isDynamic={true}
-                  ignoreCancelEvents={false}
-                >
-                  <h1
-                    style={{
-                      fontSize: "3rem",
-                      fontWeight: "bold",
-                      color: "#3ab34a",
-                      textShadow: "0.2rem 0.2rem #000",
-                      textTransform: "uppercase",
-                      textAlign: "center",
-                      writingMode: "vertical-rl",
-                      padding: "1rem",
-                    }}
-                  >
-                    {/* Kitchens */}
-                  </h1>
-                </div>
-                <div
-                  className="contact-column-2-2"
-                  activeClass="active"
-                  to="gallery-gardening"
-                  spy={true}
-                  smooth={true}
-                  hashSpy={true}
-                  duration={500}
-                  isDynamic={true}
-                  ignoreCancelEvents={false}
-                >
-                  <h1
-                    style={{
-                      fontSize: "3rem",
-                      fontWeight: "bold",
-                      color: "#3ab34a",
-                      textShadow: "0.2rem 0.2rem #000",
-                      textTransform: "uppercase",
-                      textAlign: "center",
-                      writingMode: "vertical-rl",
-                      padding: "1rem",
-                    }}
-                  >
-                    {/* Gardens */}
-                  </h1>
-                </div>
+              <div style={{ flex: 5 }}>
+                <h5>coventryfreshhome</h5>
               </div>
             </div>
-            <div className="contact-landing-footer"></div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flex: 1,
+                  // backgroundColor: "red",
+                  justifyContent: "center",
+                }}
+              >
+                <i class="fas fa-envelope-open-text fa-2x"></i>
+              </div>
+              <div style={{ flex: 5 }}>
+                <h5>coventryfreshhome@gmail.com</h5>
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flex: 1,
+                  // backgroundColor: "red",
+                  justifyContent: "center",
+                }}
+              >
+                <i class="fas fa-phone fa-2x"></i>
+              </div>
+              <div style={{ flex: 5 }}>
+                <h5>+44 (0) 737 987 5115</h5>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: "flex", flex: 4 }}>
+            <div
+              style={{
+                display: "flex",
+                flex: 1,
+                margin: "-2vh",
+                flexDirection: "column",
+              }}
+            >
+              <div className="contact-flex-container">
+                <div
+                  className="contact-column-1"
+                  // style={{ padding: "10vh 0 10vh 10vh" }}
+                >
+                  <div
+                    className="contact-column-1-1"
+                    activeClass="active"
+                    to="gallery-bathroom"
+                    spy={true}
+                    smooth={true}
+                    hashSpy={true}
+                    duration={500}
+                    isDynamic={true}
+                    ignoreCancelEvents={false}
+                  ></div>
+                  <div
+                    className="contact-column-1-2"
+                    activeClass="active"
+                    to="gallery-bedroom"
+                    spy={true}
+                    smooth={true}
+                    hashSpy={true}
+                    duration={500}
+                    isDynamic={true}
+                    ignoreCancelEvents={false}
+                  ></div>
+                </div>
+                <div
+                  className="contact-column-2"
+                  // style={{ padding: "10vh 10vh 10vh 0vh" }}
+                >
+                  <div
+                    className="contact-column-2-1"
+                    activeClass="active"
+                    to="gallery-kitchen"
+                    spy={true}
+                    smooth={true}
+                    hashSpy={true}
+                    duration={500}
+                    isDynamic={true}
+                    ignoreCancelEvents={false}
+                  ></div>
+                  <div
+                    className="contact-column-2-2"
+                    activeClass="active"
+                    to="gallery-gardening"
+                    spy={true}
+                    smooth={true}
+                    hashSpy={true}
+                    duration={500}
+                    isDynamic={true}
+                    ignoreCancelEvents={false}
+                  ></div>
+                </div>
+              </div>
+              <div className="contact-landing-footer"></div>
+            </div>
           </div>
         </div>
       </div>
