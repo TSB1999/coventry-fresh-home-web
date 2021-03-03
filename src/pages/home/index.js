0d608458ab1b6c36905ef5e16255573d6de6d791
Tobi Shitta-Bey <tsb99@andreis-mbp.wifi-res.warwick.ac.uk>
2021-03-03 10:07:21 +0000

    i guess all i had to do was take this shit a lil more serious

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -946,7 +946,7 @@ export default function Home() {
                 </Form>
                 </div>
                 <div
-                  className = "cleaning-right-right"
+                  className = "garden-right-right"
                 >
                   
                 </div>
